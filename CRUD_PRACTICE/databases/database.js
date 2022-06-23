ba6d4106f2e8ca351f63f800992b7fa4773c5352
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-06-24 05:13:20 +0900

    Update database.js

--- CRUD_PRACTICE/databases/database.js
+++ CRUD_PRACTICE/databases/database.js
@@ -1,11 +1,13 @@
 const maria = require('mysql');
+const pw = require('./mypw');
 
+//DB 모듈 불러오기 
 const conn = maria.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'##',
-    database:'gal_db',
-    dateStrings: true
+    host        :'localhost',
+    user        :'root',
+    password    : pw , // 해킹 ㄷㄷ 무서워서 빼놈 //pw는 안올릴거임 
+    database    :'gal_db',
+    dateStrings : true
 })
-
+//모듈을 conn으로 
 module.exports = conn;
