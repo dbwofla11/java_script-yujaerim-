37e56e0dc2c5502f20ac1bb0a0f5c6dea85c0831
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-06-24 04:10:55 +0900

    Update database.js

--- CRUD_PRACTICE/databases/database.js
+++ CRUD_PRACTICE/databases/database.js
@@ -3,7 +3,7 @@ const maria = require('mysql');
 const conn = maria.createConnection({
     host:'localhost',
     user:'root',
-    password:'m8161516',
+    password:'##',
     database:'gal_db',
     dateStrings: true
 })
